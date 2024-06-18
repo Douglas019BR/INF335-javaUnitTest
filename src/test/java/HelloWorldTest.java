eba29eb891a08ea676e43a86eb4d3f055498e802
DOUG <douglassermarini@gmail.com>
2024-06-17 21:50:36 -0300

    first commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void verifyJunit(){
+        Assertions.assertTrue(true);
+    }
+}
